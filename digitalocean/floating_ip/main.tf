13251dad71c3c03469fabea274d9050ae4774535
deadCat <deadcat@bleh.wtf>
2017-12-27 16:29:51 -0500

    ok. region is required but id takes precedence

--- digitalocean/floating_ip/main.tf
+++ digitalocean/floating_ip/main.tf
@@ -1,3 +1,4 @@
 resource "digitalocean_floating_ip" "my_floating_ip" {
   droplet_id = "${var.my_droplet_id}"
+  region = "${var.my_region}"
 }
--- digitalocean/floating_ip/variables.tf
+++ digitalocean/floating_ip/variables.tf
@@ -1,1 +1,2 @@
 variable "my_droplet_id" {}
+variable "my_region" {}
