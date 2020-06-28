e5c690624a88b5c8accd2a192f9f28b7f29b44c1
bleh <bleh@bleh>
2020-06-28 18:58:14 +0000

    force string

--- digitalocean/dns_record/main.tf
+++ digitalocean/dns_record/main.tf
@@ -2,5 +2,5 @@ resource "digitalocean_record" "my_record" {
   domain            = var.dns_domain
   type              = var.dns_record_type
   name              = var.dns_record_name
-  value             = var.dns_record_value
+  value             = "${var.dns_record_value}"
 }
