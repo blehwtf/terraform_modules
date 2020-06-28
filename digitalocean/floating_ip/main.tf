0ce907647d1ad7e2beaab19d20d75660fb1f1d89
bleh <bleh@bleh>
2020-06-28 19:08:28 +0000

    interpolation not needed

--- digitalocean/floating_ip/main.tf
+++ digitalocean/floating_ip/main.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_floating_ip" "my_floating_ip" {
-  droplet_id = "${var.my_droplet_id}"
-  region = "${var.my_region}"
+  droplet_id = var.my_droplet_id
+  region = var.my_region
 }
