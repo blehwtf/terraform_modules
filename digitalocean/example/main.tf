5d7009303e73589366e0c0dda02029ee2fa1f174
bleh4daze <bleh@bleh.wtf>
2021-05-12 15:11:03 -0400

    terraform-13-upgrade

--- digitalocean/example/main.tf
+++ digitalocean/example/main.tf
@@ -1,22 +1,18 @@
-provider "digitalocean" {
-  token             = "${var.do_token}" // from env
-}
-
 module "example_droplet" {
   source            = "github.com/blehwtf/terraform_modules.git//digitalocean/droplet"
-  droplet_image     = "${var.droplet_image}"
-  fqdn              = "${var.fqdn}"
-  ssh_keys          = "${var.ssh_keys}"
-  droplet_region    = "${var.droplet_region}"
-  droplet_size      = "${var.droplet_size}"
-  user_data_file    = "${var.user_data_file}"
-  tags              = "${var.tags}"
+  droplet_image     = var.droplet_image
+  fqdn              = var.fqdn
+  ssh_keys          = var.ssh_keys
+  droplet_region    = var.droplet_region
+  droplet_size      = var.droplet_size
+  user_data_file    = var.user_data_file
+  tags              = var.tags
 }
 
 module "example_dns_record" {
   source            = "github.com/blehwtf/terraform_modules.git//digitalocean/dns_record"
-  dns_domain        = "${var.dns_domain}"
+  dns_domain        = var.dns_domain
   dns_record_type   = "A"
-  dns_record_name   = "${var.fqdn}."
-  dns_record_value  = "${module.example_droplet.ip}"
+  dns_record_name   = var.fqdn
+  dns_record_value  = module.example_droplet.ip
 }
